0f93d5e0faf671d5c3fe9aeccc07cb844087c312
gituser <2500376955@qq.com>
2022-08-18 13:59:16 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/guaniu/app/mylibrary/MavenTest.java
@@ -0,0 +1,14 @@
+package com.guaniu.app.mylibrary;
+
+/**
+ * @program: GitMaven
+ * @description:
+ * @author: W
+ * @create: 2022-08-18 13:51
+ **/
+public class MavenTest {
+    public MavenTest() {
+    }
+
+    public void call(){}
+}
